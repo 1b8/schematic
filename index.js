c296efb7c0862d0b04b2531a59acf811542ae976
1b8 <kronosville@gmail.com>
2015-11-14 19:33:23 +1300

    Add entities

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-var fs = require('fs'),
-    nbt = require('prismarine-nbt'),
-    vec3 = require('vec3');
+var fs = require('fs');
+var util = require('util');
+var nbt = require('prismarine-nbt');
+var vec3 = require('vec3');
 
 module.exports = function (version) {
   var Block = require('prismarine-block')(version);
+  var mcData = require('minecraft-data')(version);
+
+  Schematic.read = function (path, callback) {
+    if (!callback) callback = function (err) {
+      if (err) throw err;
+    }
+
+    fs.readFile(path, function (err, file) {
+      if (err) callback(err);
+
+      nbt.parse(file, function (err, data) {
+        if (err) callback(err);
+        callback(null, new Schematic(data.value.value));
+      });
+    });
+  };
+
+  Schematic.promiseRead = function (path) {
+    return new Promise(function (resolve, reject) {
+      Schematic.read(path, function (err, data) {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  };
 
   function Schematic(raw) {
+    var entities = raw.Entities.value.value;
+
     this.raw = raw;
     this.width = raw.Width.value;
     this.height = raw.Height.value;
     this.length = raw.Length.value;
+
+    this.entities = [];
+    for (var i = 0; i < entities.length; i++) {
+      this.entities.push(new Entity(entities[i]));
+    }
   }
 
   Schematic.prototype.blockAt = function (x, y, z) {
@@ -19,20 +52,5 @@ module.exports = function (version) {
     return block;
   };
 
-  return {
-    read: function (path, callback) {
-      if (!callback) callback = function (err) {
-        if (err) throw err;
-      }
-
-      fs.readFile(path, function (err, file) {
-        if (err) callback(err);
-
-        nbt.parse(file, function (err, data) {
-          if (err) callback(err);
-          callback(null, new Schematic(data.value.value));
-        });
-      });
-    }
-  };
+  return Schematic;
 };
