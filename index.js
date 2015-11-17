18527b358f84773c0a472771b28c193f9a28361f
1b8 <kronosville@gmail.com>
2015-11-17 15:35:36 +1300

    Switch to ES6 class for Schematic, rename read to readFile..

--- index.js
+++ index.js
@@ -7,58 +7,58 @@ module.exports = function (version) {
   var BlockEntity = require('./lib/block-entity')(version);
   var block = require('./lib/pos')(version).block;
 
-  Schematic.read = function (path, callback) {
-    if (!callback) callback = function (err) {
-      if (err) throw err;
+  class Schematic {
+    constructor(raw) {
+      var entities = raw.Entities.value.value;
+      var blockEntities = raw.TileEntities.value.value;
+
+      this.raw = raw;
+      this.width = raw.Width.value;
+      this.height = raw.Height.value;
+      this.length = raw.Length.value;
+      this.entities = [];
+      this.blockEntities = [];
+      for (var i = 0; i < entities.length; i++)
+        this.entities.push(new Entity(entities[i]));
+      for (i = 0; i < blockEntities.length; i++) {
+        this.blockEntities.push(new BlockEntity(raw, blockEntities[i]));
+      }
     }
 
-    fs.readFile(path, function (err, file) {
-      if (err) callback(err);
+    blockAt(x, y, z) {
+      var vec = vec3(x, y, z);
 
-      nbt.parse(file, function (err, data) {
+      // Check if it's a block entity
+      var blockEntity;
+      for (var i = 0; i < this.blockEntities.length; i++) {
+        blockEntity = this.blockEntities[i];
+        if (blockEntity.position.equals(vec)) return blockEntity;
+      }
+      return block(this.raw, pos);
+    }
+
+    static read(data, callback) {
+      if (!callback) callback = function (err) {
+        if (err) throw err;
+      }
+
+      nbt.parse(data, function (err, data) {
         if (err) callback(err);
-        callback(null, new Schematic(data.value.value));
+        callback(err, new Schematic(data));
       });
-    });
-  };
+    }
 
-  Schematic.promiseRead = function (path) {
-    return new Promise(function (resolve, reject) {
-      Schematic.read(path, function (err, data) {
-        if (err) reject(err);
-        else resolve(data);
+    static readFile(path, callback) {
+      if (!callback) callback = function (err) {
+        if (err) throw err;
+      };
+
+      fs.readFile(path, function (err, data) {
+        if (err) callback(err);
+        Schematic.read(data, callback);
       });
-    });
-  };
-
-  function Schematic(raw) {
-    var entities = raw.Entities.value.value;
-    var blockEntities = raw.TileEntities.value.value;
-
-    this.raw = raw;
-    this.width = raw.Width.value;
-    this.height = raw.Height.value;
-    this.length = raw.Length.value;
-    this.entities = [];
-    this.blockEntities = [];
-    for (var i = 0; i < entities.length; i++)
-      this.entities.push(new Entity(entities[i]));
-    for (i = 0; i < blockEntities.length; i++) {
-      this.blockEntities.push(new BlockEntity(raw, blockEntities[i]));
     }
   }
-
-  Schematic.prototype.blockAt = function (x, y, z) {
-    var vec = vec3(x, y, z);
-
-    // Check if it's a block entity
-    var blockEntity;
-    for (var i = 0; i < this.blockEntities.length; i++) {
-      blockEntity = this.blockEntities[i];
-      if (blockEntity.position.equals(vec)) return blockEntity;
-    }
-    return block(this.raw, pos);
-  };
 
   return Schematic;
 };
